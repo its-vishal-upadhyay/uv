d8a211eaa1371ab7315f2701d8d814202f9a289e
Vishal <vishalupadhyay4321@gmail.com>
2024-09-23 18:20:41 +0530

    add para

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
 import React from "react"
 function About(){
     return(
-        <h1>hello world</h1>
+      <>
+          <h1>hello world</h1>
         <h1>hello react world</h1>
         <p>react first program</p>
+        <p>react first program</p>
+      </>
     );
 }
 export default About;
