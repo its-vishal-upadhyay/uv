31d64b83108cab84378d03f660bd68a5f8b0b4cd
Vishal <vishalupadhyay4321@gmail.com>
2024-09-25 16:38:18 +0530

    modify about.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,21 +2,12 @@ import './About.css'
 import React from "react"
 function About(){
     return(
-<<<<<<< HEAD
       <>
           <h1>hello world</h1>
         <h1>hello react world</h1>
         <p>react first program</p>
         <p>react first program</p>
       </>
-=======
-        <>
-        <h1>hello world</h1>
-        <h1>hello react world</h1>
-        <p>react first program</p>
-        <p>react first program</p>
-        </>
->>>>>>> bug-fix
     );
 }
 export default About;
